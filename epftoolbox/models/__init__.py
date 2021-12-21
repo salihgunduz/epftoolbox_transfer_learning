bf4d74af8b1efb1966cd3b1e8b309510d2417020
salihgunduz <gunduz.salih@gmail.com>
2021-12-21 18:55:27 +0300

    Update __init__.py

--- epftoolbox/models/__init__.py
+++ epftoolbox/models/__init__.py
@@ -1,3 +1,4 @@
 from ._lear import (LEAR, evaluate_lear_in_test_dataset)
 from ._dnn import (DNNModel, DNN, evaluate_dnn_in_test_dataset, format_best_trial)
+from ._dnn_finetune_test import (DNNModel, DNN, evaluate_finetune_in_test_dataset, format_best_trial)
 from ._dnn_hyperopt import (hyperparameter_optimizer)
